3437ad0a2fa3bdadaf73af5f9043e01f55db7e57
Shubham Goel2 <SG00617711@techmahindra.com>
2020-05-06 18:12:19 +0530

    Angular high chart broker connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+// import { Observable } from 'rxjs/Observable';
+// import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject';
+
+import * as io from 'socket.io-client';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mqttAngularSubs';
+
+  highcharts = Highcharts; // required
+  chartConstructor = 'chart'; // optional string, defaults to 'chart'
+  chartOptions = {
+    chart: {
+      zoomType: 'x'
+    },
+
+    title: {
+      text: 'Vibration Analysis'
+    },
+    subtitle: {
+      text: 'Amplitude vs Frequency chart'
+    },
+
+    series: [{
+      data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    }],
+
+    xAxis: {
+      tickInterval: 0.5,
+      gridLineWidth: 1,
+      title: {
+        enabled: true,
+        text: 'Frequency',
+        style: {
+          fontWeight: 'normal'
+        }
+      }
+    },
+
+    yAxis: {
+      title: {
+        enabled: true,
+        text: 'Amplitute',
+        style: {
+          fontWeight: 'normal'
+        }
+      }
+    },
+
+    annotations: [{
+      labels: [{
+        point: {
+          x: 3,
+          y: 129.2,
+          xAxis: 0,
+          yAxis: 0
+        },
+        text: 'x: {x}<br/>y: {y}'
+      },
+      {
+        point: {
+          x: 0,
+          y: 0
+        },
+        text: 'x: {point.plotX} px<br/>y: {point.plotY} px'
+      },
+      {
+        point: {
+          x: 5,
+          y: 100,
+          xAxis: 0
+        },
+        text: 'x: {x}<br/>y: {point.plotY} px'
+      }],
+      labelOptions: {
+        x: 40, y: -10
+      }
+    }]
+  }; // required
+  chartCallback = function (chart) { } // optional function, defaults to null
+  updateFlag = false; // optional boolean
+  oneToOneFlag = true; // optional boolean, defaults to false
+  runOutsideAngular = false; // optional boolean, defaults to false
+
+
+  topic = 'LINTANGtest123'
+  private url = 'http://localhost:2222';
+  private socket;
+  constructor() {
+    this.socket = io(this.url);
+  }
+
+
+  ngOnInit() {
+    // setInterval(() => {
+    //   // this.highcharts
+    //   console.log(this.updateFlag, this.chartOptions.series[0].data)
+    //   this.chartOptions.series[0].data.push(100)
+    //   this.updateFlag = true;
+    //   console.log(this.updateFlag, this.chartOptions.series[0].data)
+    // }, 3000);
+
+    this.socket.on('sendData', (message) => {
+      this.chartOptions.series[0].data.push(message)
+      this.updateFlag = true;
+      console.log(message);
+    });
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 import { AppComponent } from './app.component';
+// import { MqttModule, MqttServiceConfig, MqttService, MqttClientService } from 'ngx-mqtt';
+// import { NgxMqttClientModule } from 'ngx-mqtt-client';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HighchartsChartModule,
+    // MqttModule,
+    // MqttServiceConfig,
+    // NgxMqttClientModule.withOptions({
+    //   // manageConnectionManually: true, //this flag will prevent the service to connection automatically
+    //   host: 'localhost',
+    //   protocol: 'mqtt',
+    //   port: 1234,
+    //   // path: '/mqtt'
+    // })
   ],
-  providers: [],
+  providers: [
+    //   { provide: MqttServiceConfig, useValue: {} },
+    // // MqttService,
+    // { provide: MqttClientService, useValue: {} }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
